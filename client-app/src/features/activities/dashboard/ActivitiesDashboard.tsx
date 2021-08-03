ca33a3cb9cb2bde25dc25fc072d8259b9ee9a605
LukeMajeske <lmajeske@gmail.com>
2021-08-03 00:17:13 -0500

    Locally Delete and Create activities

---
+++ client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Grid} from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+import ActivityList from "./ActivityList";
+
+interface Props{
+    activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean;
+    formOpen: (id : string) => void;
+    formClose: () => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
+}
+
+export default function ActivityDashboard({activities,selectedActivity,selectActivity,cancelSelectActivity,
+editMode, formOpen, formClose, createOrEdit, deleteActivity}:Props){
+    return(
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList activities={activities} 
+                selectActivity={selectActivity}
+                deleteActivity = {deleteActivity} ></ActivityList>
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && !editMode &&
+                <ActivityDetails 
+                    activity={selectedActivity} 
+                    cancelSelectActivity = {cancelSelectActivity}
+                    formOpen={formOpen}
+                    />}
+                {editMode &&
+                <ActivityForm formClose={formClose} activity={selectedActivity} createOrEdit = {createOrEdit}/>}
+            </Grid.Column>
+        </Grid>
+    )
+}
